3e8d003adf2fd2bdba286f356ac473ef48938db3
Dimaldos <dima@hakobyan.ru>
2024-04-28 21:31:03 +0300

    second type of db access via repository

--- catalogue-service/src/main/java/ru/dimaldos/selmag/catalogueservice/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ru/dimaldos/selmag/catalogueservice/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package ru.dimaldos.selmag.catalogueservice.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.dimaldos.selmag.catalogueservice.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    Iterable<Product> findAllByTitleLikeIgnoreCase(String filter);
+    @Query(value = "select p from Product p where p.title ilike :filter")
+    Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
