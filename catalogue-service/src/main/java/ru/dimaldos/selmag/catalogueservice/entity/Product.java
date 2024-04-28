1eb80f619d77bfc1d42ed18b311c56b6a4c1dd47
Dimaldos <dima@hakobyan.ru>
2024-04-28 21:42:31 +0300

    third type of db access via repository

--- catalogue-service/src/main/java/ru/dimaldos/selmag/catalogueservice/entity/Product.java
+++ catalogue-service/src/main/java/ru/dimaldos/selmag/catalogueservice/entity/Product.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -18,6 +20,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(schema = "catalogue", name = "t_product")
+@NamedQueries(
+        @NamedQuery(
+                name = "Product.findAllByTitleLikeIgnoringCase",
+                query = "select p from Product p where p.title ilike :filter"
+        )
+)
 public class Product {
 
     @Id
--- catalogue-service/src/main/java/ru/dimaldos/selmag/catalogueservice/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ru/dimaldos/selmag/catalogueservice/repository/ProductRepository.java
@@ -7,6 +7,6 @@ import ru.dimaldos.selmag.catalogueservice.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    @Query(value = "select * from catalogue.t_product where c_title ilike :filter", nativeQuery = true)
+    @Query(name = "Product.findAllByTitleLikeIgnoringCase", nativeQuery = true)
     Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
