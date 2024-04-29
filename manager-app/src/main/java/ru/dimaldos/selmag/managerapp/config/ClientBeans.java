932aaa24d1728c2faf38e8960460c6c44add3030
Dimaldos <dima@hakobyan.ru>
2024-04-29 20:18:31 +0300

    аутентификация

--- manager-app/src/main/java/ru/dimaldos/selmag/managerapp/config/ClientBeans.java
+++ manager-app/src/main/java/ru/dimaldos/selmag/managerapp/config/ClientBeans.java
@@ -3,6 +3,7 @@ package ru.dimaldos.selmag.managerapp.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import ru.dimaldos.selmag.managerapp.client.DefaultProductsRestClient;
 
@@ -10,9 +11,14 @@ import ru.dimaldos.selmag.managerapp.client.DefaultProductsRestClient;
 public class ClientBeans {
 
     @Bean
-    public DefaultProductsRestClient productsRestClient(@Value("${selmag.services.catalogue.uri:http://localhost:8082}") String catalogueBaseUri) {
+    public DefaultProductsRestClient productsRestClient(
+            @Value("${selmag.services.catalogue.uri:http://localhost:8082}") String catalogueBaseUri,
+            @Value("${selmag.services.catalogue.username:}") String catalogueUsername,
+            @Value("${selmag.services.catalogue.password:}") String cataloguePassword
+    ) {
         return new DefaultProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
                 .build());
     }
 }
